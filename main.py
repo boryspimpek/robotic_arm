bb61e5a702132d320776171b8c1433fd8361f874
borysgame <borysgame@gmail.com>
2025-05-10 20:04:48 +0200

    q

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 # robot_arm/main.py
 
 import time
+
+from matplotlib import pyplot as plt
 from kinematics import Kinematics
-from kinematics_full import ik_to_servo_angles, solve_ik_3d
+from kinematics_full import FullKinematics
 from servos import ServoController
 from controller import ArmController
 from sc_controll import open_gripper, sc_servo, close_gripper, sc_servo_position
-from config import L1, L2, gripper, wrist, port, base, shoulder, elbow
+from config import L1, L2, L3, gripper, wrist, port, base, shoulder, elbow
 
 kin = Kinematics(L1, L2)
+fullkin = FullKinematics(L1, L2, L3)
 servo_ctrl = ServoController(port)
 arm = ArmController(kin, servo_ctrl)
 
 
-angles, positions = solve_ik_3d(150, 0, 200)
-end_angles = ik_to_servo_angles(angles)
+angles, positions = fullkin.solve_ik_3d(150, 50, 200)
+end_angles = fullkin.ik_to_servo_angles(angles)
 
 for sid, angle in end_angles.items():
     print(f"  Servo {sid}: {angle:.2f}°")
+
+plt.show()
+
 
 start_angles = servo_ctrl.get_all_servo_positions_deg([base, shoulder, elbow, wrist])
 
