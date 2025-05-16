84ae3934a6783946f0215e62c2e1b987d723ca5c
borysgame <borysgame@gmail.com>
2025-05-16 15:28:58 +0200

    ert

--- main.py
+++ main.py
@@ -15,33 +15,37 @@ fullkin = FullKinematics(L1, L2, L3)
 servo_ctrl = ServoController(port)
 arm = ArmController(kin, servo_ctrl)
 
+servo_ctrl.move_servo(base, 90)
+servo_ctrl.move_servo(shoulder, 90)
+servo_ctrl.move_servo(elbow, 90)
+servo_ctrl.move_servo(wrist, 90)
 
-a = "min_angle_sum"
-b = "flat"
-c = "vertical_up"
-d = "vertical_down"
-e = "standard"
-tempo_dps=60
+# a = "min_angle_sum"
+# b = "flat"
+# c = "vertical_up"
+# d = "vertical_down"
+# e = "standard"
+# tempo_dps=60
 
 
-def move_arm_sequentially(start_x, end_x, y, z, tempo_dps, cost_mode):
+# def move_arm_sequentially(start_x, end_x, y, z, tempo_dps, cost_mode):
 
-    step = 5 if start_x <= end_x else -5
-    for x in range(start_x, end_x + step, step):
-        arm.move_to_point_ik_full(x, y, z, tempo_dps, cost_mode)
+#     step = 5 if start_x <= end_x else -5
+#     for x in range(start_x, end_x + step, step):
+#         arm.move_to_point_ik_full(x, y, z, tempo_dps, cost_mode)
 
-# Call the function
-while True:
-    a = "min_angle_sum"
-    b = "flat"
-    c = "vertical_up"
-    d = "vertical_down"
-    e = "standard"
-    tempo_dps=160
-    cost_mode = d
+# # Call the function
+# while True:
+#     a = "min_angle_sum"
+#     b = "flat"
+#     c = "vertical_up"
+#     d = "vertical_down"
+#     e = "standard"
+#     tempo_dps=160
+#     cost_mode = d
 
-    move_arm_sequentially(240, 70, 0, -50, tempo_dps, cost_mode)
-    move_arm_sequentially(70, 240, 0, -50, tempo_dps, cost_mode)
+#     move_arm_sequentially(240, 70, 0, -50, tempo_dps, cost_mode)
+#     move_arm_sequentially(70, 240, 0, -50, tempo_dps, cost_mode)
 
 # arm.move_to_point_ik_full(x, 0, 0, tempo_dps, cost_mode="standard")
 
