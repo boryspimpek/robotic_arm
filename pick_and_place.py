bcaba706f5668273521859d40c23de23b449cc08
borysgame <borysgame@gmail.com>
2025-05-23 18:34:24 +0200

    pickdrop

--- pick_and_place.py
+++ pick_and_place.py
@@ -6,42 +6,46 @@ from kinematics_full import FullKinematics
 from sc_controll import close_gripper, open_gripper
 from servos import ServoController
 
+arm = ArmController(
+    kinematics=Kinematics(L1, L2),
+    servo_ctrl=ServoController(port_bus),
+    fullkin=FullKinematics(L1, L2, L3)
+)
 
-arm = ArmController(kinematics=Kinematics(L1, L2), servo_ctrl=ServoController(port_bus), fullkin=FullKinematics(L1, L2, L3))
-
-
-
-
-def pickup_place(pickup, place, tempo_dps=60, lift_height=40, cost_mode="vertical_down"):
-    x1, y1, z1 = pickup
-    x2, y2, z2 = place
+def pickup_place(pickup_point, place_point, lift_height=40):
+    x1, y1, z1 = pickup_point
+    x2, y2, z2 = place_point
     above_pickup = (x1, y1, z1 + lift_height)
     above_place = (x2, y2, z2 + lift_height)
 
-    def move_to(position, tempo_dps=tempo_dps, cost_mode=cost_mode):
-        arm.move_to_point_full(*position, tempo_dps, cost_mode)
-        time.sleep(0.5)
-
-    def grip(action):
-        if action == "open":
-            open_gripper()
-        elif action == "close":
-            close_gripper()
-        time.sleep(0.5)
-
-    # Sekwencja pick & place
     move_to(above_pickup)
-    grip("open")
-    move_to(pickup, tempo_dps=30)
-    grip("close")
-    move_to(above_pickup)
+    perform_pick(pickup_point, above_pickup)
     move_to(above_place)
-    move_to(place, tempo_dps=30)
-    grip("open")
-    move_to(above_place)
-    grip("close")
+    perform_place(place_point, above_place)
     arm.homepos()
 
-pickup_place((150, 0, -110), (150, -100, -110), lift_height=30)
-time.sleep(1)
-pickup_place((150, -100, -110), (150, 0, -110), lift_height=30)
+def move_to(position, tempo_dps=60, cost_mode="vertical_down"):
+    arm.move_to_point_full(*position, tempo_dps, cost_mode)
+    time.sleep(0.5)
+
+def perform_pick(pick_point, above_point, tempo_dps=30, cost_mode="vertical_down"):
+    open_gripper()
+    time.sleep(0.5)
+    arm.move_to_point_full(*pick_point, tempo_dps, cost_mode)
+    time.sleep(0.5)
+    close_gripper()
+    time.sleep(0.5)
+    arm.move_to_point_full(*above_point, tempo_dps, cost_mode)
+    time.sleep(0.5)
+
+def perform_place(place_point, above_point, tempo_dps=30, cost_mode="vertical_down"):
+    arm.move_to_point_full(*place_point, tempo_dps, cost_mode)
+    time.sleep(0.5)
+    open_gripper()
+    time.sleep(0.5)
+    arm.move_to_point_full(*above_point, tempo_dps, cost_mode)
+    time.sleep(0.5)
+    close_gripper()
+
+# Przykładowe wywołanie
+pickup_place((150, -100, -110), (200, -100, -50))
